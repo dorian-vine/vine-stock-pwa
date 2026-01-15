580c20956e7111c838524f3714aa5a7b1e6df10a
dorian-vine <dorian.leger92@gmail.com>
2026-01-15 03:26:11 +0100

    Add files via upload

---
+++ sw_V5_2.js
@@ -0,0 +1,7 @@
+const CACHE='vine-stock-v5.2';
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js'])));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
