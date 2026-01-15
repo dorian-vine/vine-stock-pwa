847579f6997f80f84cfa55546a8f2d878d770e5c
dorian-vine <dorian.leger92@gmail.com>
2026-01-15 03:19:11 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vine-stock-v5';
+const CACHE='vine-stock-v5.2';
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js'])));
 });
